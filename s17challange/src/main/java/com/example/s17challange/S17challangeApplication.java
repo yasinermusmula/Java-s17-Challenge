8fefcce44bda4d114a7de7b7277ddff216db2116
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-02-10 15:03:50 +0300

    task crreated

---
+++ s17challange/src/main/java/com/example/s17challange/S17challangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.s17challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17challangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S17challangeApplication.class, args);
+	}
+
+}
---
+++ s17challange/src/test/java/com/example/s17challange/S17challangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s17challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S17challangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
